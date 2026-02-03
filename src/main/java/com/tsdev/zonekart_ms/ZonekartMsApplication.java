2d5970399b1ac28fecb99663da343891f6df4cdc
tamilstgit <tamils088888@gmail.com>
2026-02-03 15:03:46 +0530

    Initial commit

---
+++ src/main/java/com/tsdev/zonekart_ms/ZonekartMsApplication.java
@@ -0,0 +1,13 @@
+package com.tsdev.zonekart_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZonekartMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZonekartMsApplication.class, args);
+	}
+
+}
